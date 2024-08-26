9c40ee37490ae8d913a5e05f9aaba86647651a3d
Kariny Abrahao <kariny_abrahao@hotmail.com>
2024-08-26 18:46:12 -0300

    file analytics

---
+++ view/paymentAnalytics.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Menu
+from tkinter import Label
+from tkinter import Entry
+from tkinter import Button
+from model.tollPayment import TollPayment
+from control.maincontrol import MainControl
+
+
+class PaymentAnalytics:
+    def any():
+        return
